1386244d844037a315f70a2810e05e2f6dc9f71b
Jesse Maxim <jessmaxim303@gmail.com>
2019-06-30 23:49:05 -0600

    Add click buttom fuction

---
+++ main.js
@@ -0,0 +1,12 @@
+// var topInput = document.querySelector(‘.top-input’);
+// var topButton = document.querySelector(‘.top-button’);
+// var someDogs = document.querySelector(‘.some-dogs’);
+
+
+
+// topButton.addEventListener(“click”, changeName);
+
+// function changeName(){
+//  event.preventDefault();
+//  someDogs.innerHTML = topInput.value;
+// }
