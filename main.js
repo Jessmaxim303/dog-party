e0644ee2dc7e21a47c3322d6eb0f3149ae4a6d92
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-01 12:37:54 -0600

    add link for js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-// var topInput = document.querySelector(‘.top-input’);
-// var topButton = document.querySelector(‘.top-button’);
-// var someDogs = document.querySelector(‘.some-dogs’);
+console.log("hello")
+
+var topInput = document.querySelector("#top-input");
+var topButton = document.querySelector(".welcome-button");
+var someDogs = document.querySelector("#some-dogs");
 
 
+topButton.addEventListener('click', changeName);
 
-// topButton.addEventListener(“click”, changeName);
-
-// function changeName(){
-//  event.preventDefault();
-//  someDogs.innerHTML = topInput.value;
-// }
+function changeName(event){
+	console.log("hello")
+ event.preventDefault();
+ someDogs.innerHTML = topInput.value;
+}
